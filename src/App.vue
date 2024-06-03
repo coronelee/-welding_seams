30e218fd52a461fbfe6f6a17251c4b45f3e57fac
coronelee <a.tirki90@gmail.com>
2024-06-03 23:21:26 +0300

    f

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-screen bg-gray-500 h-screen flex flex-col justify-end items-center">
+    <div class="w-[90%] h-[90%] bg-gray-100"> </div>
+    <div class="  [&>button]:w-12 [&>button]:h-12 [&>button]:bg-gray-100 [&>button]:text-white [&>button]:m-2 [&>button]:rounded-[50%]"> 
+      <button></button><button></button><button></button><button></button> 
+    </div>
+  </div>
+</template>
