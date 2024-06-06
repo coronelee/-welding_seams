1baeb87c432ff81226818adefbbc649a6de19ee2
coronelee <a.tirki90@gmail.com>
2024-06-07 02:53:43 +0300

    ad

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,76 @@
-<!-- <script setup>
-import { ref } from 'vue';
-
-const imageSrc = ref("");
-const onFileChange = (event) => {
-  const selectedFile = event.target.files[0];
-  imageSrc.value = URL.createObjectURL(selectedFile);
-}
-</script> -->
+ 
 <script setup>
   import { ref } from 'vue';
- 
-  const imageSrc = ref("");
- 
-  
+  import HelpComponent from './components/HelpComponent.vue';
+  const imageSrc = ref(null);
+  const height = ref('33%')
+  const page = ref(null)
+  const image = ref('/picture/butt.png')
+
+  const editImage = (img) => {
+     image.value = img
+  }
+
   const onFileChange = (event) => {
     const selectedFile = event.target.files[0];
     imageSrc.value = URL.createObjectURL(selectedFile);
-    console.log(imageSrc.value)
   } 
 
-  const downloadImage = () => {
-    document.getElementById('file').click();
+  const downloadImage = (id) => {
+    document.getElementById(""+id).click();
+    height.value = '100%'
+    page.value = "image"
   }
+   
+const openQuePage = () => {
+  height.value = '66%'
+  page.value = "que"
+}
+
 </script>
 <template>
 <input type="file" id="file" class="hidden" @change="onFileChange">
 <input type="file" id="camera" accept="image/*" capture="camera" class="hidden" @change="onFileChange">
 
-  <div class="w-screen h-screen bg-gray-500 ">
-    <div v-if="!imageSrc" class="w-full h-full flex flex-col justify-center items-center text-white [&>div]:border [&>div]:border-gray-300 [&>div]:p-2 [&>div]:m-2 [&>div]:rounded-xl [&>div]:bg-gray-600">
-      <div onclick=" document.getElementById('camera').click(); ">Сделать снимок</div>
-      <div @click="downloadImage">Загрузить картинку</div>
+  <div class="w-screen h-screen bg-[#19173d] flex flex-col justify-end items-end">
+    <div v-if="page === 'que'" class="w-full h-[33%] flex justify-center items-center p-8">
+    <img :src="image"  alt="" class="w-full ">
+
+    </div>
+    <div :style="`height:` + height"  class="bg-[#252350] w-full transition-all duration-1000  border-t border-[#393677] rounded-t flex justify-center items-center flex gap-8 flex-col p-8
+                [&>button]:text-white [&>button]:text-2xl [&>button]:p-2 [&>button]:rounded-xl [&>button]:border [&>button]:border-[#393677] [&>button]:px-4 [&>button]:py-2 [&>button]:bg-[#252350]">
+          <button v-if="!imageSrc" @click="downloadImage('file')">Загрузить фотографию</button>
+          <button v-if="!imageSrc" @click="downloadImage('camera')">Сделать фотографию</button>
+
+          <div v-if="imageSrc" class="w-full h-full flex flex-col justify-center items-center relative gap-12">
+          
+          <img v-if="page == 'image'" :src="imageSrc" class="  " alt="Uploaded Image">
+
+          <component v-if="page === 'que'" :is="HelpComponent" :editImage="editImage"/>
+          <div class="w-full h-20 flex justify-between absolute bottom-0 [&>div>button]:rounded-full [&>div>button]:bg-[#252350] [&>div>button]:w-20 [&>div>button]:h-20 [&>div>span]:text-white">
+            <div class="flex flex-col justify-between items-center ">
+              <button @click="downloadImage('file')" class="bg-[url('/svg/download.svg')] bg-contain bg-center bg-no-repeat"></button>
+              <span>Загрузить</span>
+            </div>
+            <div class="flex flex-col justify-between items-center ">
+              <button @click="openQuePage" class="bg-[url('/svg/que.svg')] bg-contain bg-center bg-no-repeat"></button>
+              <span>Помощь</span>
+            </div>
+            <!-- <div class="flex flex-col justify-between items-center ">
+              <button @click="downloadImage('file')" class="bg-[url('/svg/download.svg')] bg-contain bg-center bg-no-repeat"></button>
+              <span>zxc</span>
+            </div>
+            <div class="flex flex-col justify-between items-center ">
+              <button @click="downloadImage('file')" class="bg-[url('/svg/download.svg')] bg-contain bg-center bg-no-repeat"></button>
+              <span>zxc</span>
+            </div> -->
+          </div>
+        </div>
+    </div>
+
     
-      
-    </div>
-    <div v-if="imageSrc" class="w-full h-full p-8 flex flex-col justify-center items-center">
-      <div class="w-[90%] h-[90%] bg-gray-100" >
-        <img class="w-full h-full " v-if="imageSrc" :src="imageSrc" alt="Uploaded Image">
-      </div>
-      <div class="[&>button]:w-12 [&>button]:h-12 [&>button]:bg-gray-100 [&>button]:text-white [&>button]:m-2 [&>button]:rounded-[50%] [&>button]:bg-center [&>button]:bg-no-repeat [&>button]:bg-contain">
-        <button @click="downloadImage" class="bg-[url('/svg/download.svg')] "></button ><button class="bg-[url('/svg/que.svg')] "></button><button></button><button></button>
-      </div>
-    </div>
+  
   </div>
 </template>
 
-
+ 
---
+++ src/components/HelpComponent.vue
@@ -0,0 +1,53 @@
+<script setup>
+const props = defineProps({
+    editImage: Function
+})
+import { ref } from 'vue';
+
+const editImg = (img) => {
+    props.editImage(img)
+}
+
+
+const count = ref(2)
+const arr = 
+    [
+        {   
+            id: 0,
+            name:'Стыковой шов',
+            img: '/picture/butt.png',
+            discription: 'Наиболее распространенный метод сварки, который используется для соединения листов металла и торцевых частей труб. Требованием к стыковым швам является жесткое соединение двух заготовок с допустимым зазором величиной не более 2 мм, заполняемым расплавленным металлом, образующимся при плавлении электрода.'
+        },
+        {
+            id: 1,
+            name: 'Угловой шов',
+            img: '/picture/angular.jpg',
+
+            discription:'Этот вид соединения довольно сложен в исполнении, так как в процессе сварки расплавленный металл имеет свойство стекать с угловой поверхности – с вертикальной части заготовки на горизонтальную. Чтобы справиться с такой проблемой, сварщику необходимо держать электрод под правильным углом наклона.'
+        },
+        {
+            id: 2,
+            name: 'Тавровый шов',
+            img: '/picture/tavrovoe.jpg',
+            discription:'Если выполнить разрез такого соединения, то можно увидеть, что одна деталь стыкуется с другой по профилю, выполненным в виде буквы «Т». По ГОСТу существует 9 видов исполнения тавровых швов, для которых требуется глубокое проплавление, выполняемое путем применения автоматического вида сварки.'
+        }
+    ]
+</script>
+
+
+
+
+<template>
+    <div class="h-3/4 w-full  absolute top-0 flex flex-col">
+        <span class="text-3xl text-white">{{ arr[count].name }}</span>
+        <span class="text-xl text-[#abaeb5] mt-4">{{ arr[count].discription }}</span>
+    </div>
+    <div class="h-[15px] w-full  absolute bottom-24 flex justify-center items-center [&>img]:w-[45px] [&>img]:h-[45px]  [&>div]:w-[15px] [&>div]:h-[15px] [&>div]:rounded-full   [&>div]:border [&>div]:border-[#393677] [&>div]:m-2 [&>div]:bg-[#252350]">
+        <img src="/svg/left.svg" alt="" @click="count = count - 1 < 0 ? 2 : count - 1, editImg(arr[count].img)">
+        <div :style="count == 0 ? 'background-color: #393677' : ''"></div>
+        <div :style="count == 1 ? 'background-color: #393677' : ''"></div>
+        <div :style="count == 2 ? 'background-color: #393677' : ''"></div>
+        <img src="/svg/right.svg" alt="" @click="count = count + 1 > 2 ? 0 : count + 1, editImg(arr[count].img)">
+
+    </div>
+</template>
